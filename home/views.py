b7c70129b52d214f4d2c62fb6165b91cecf37f14
jairnarvaez <jairandresnar@gmail.com>
2025-09-26 20:13:21 -0500

    Se corrigío el bug de la vista del home para el despliegue

--- home/views.py
+++ home/views.py
@@ -3,26 +3,19 @@ from .models import HomeConfig
 import requests
 from collections import Counter
 from django.conf import settings
-
+from machines.models import Machine
 
 def home_view(request):
-    config = HomeConfig.objects.first()  
-
-    api_url = f"{settings.API_BASE_URL}/machines-basic/"
-
-    response = requests.get(api_url)
-    machines = response.json() if response.status_code == 200 else []
-
+    config = HomeConfig.objects.first()      
+    machines = Machine.objects.all().values("difficulty")
     difficulties = [m["difficulty"] for m in machines if m.get("difficulty")]
     counts = Counter(difficulties)
-
     stats = {
         "total": len(machines),
         "easy": counts.get("Easy", 0),
         "medium": counts.get("Medium", 0),
         "hard": counts.get("Hard", 0),
     }
-
     context = {
         "config": config,
         "stats": stats,
